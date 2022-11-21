65601103b56f3efef344861e66640d09c7c42305
mauricior-banega <100450611+mauricior-banega@users.noreply.github.com>
2022-11-20 23:59:27 -0300

    Creando modelos (añadido)
    -Le agregamos comentarios para explicar mas en detalle nuestro código de conexión.

--- backendmvc/modelos/administrador.modelo.php
+++ backendmvc/modelos/administrador.modelo.php
@@ -2,6 +2,8 @@
 
 Aclaraciones: En este archivo vamos a hacer las conexiones  que nos van a servir para loguearnos, para la conexion, para mostrar,crear y eliminar los usuarios. 
 
+-Se crea una variable llamada $stmt que es sinonimos de statement que significa "declaracion", puede usarse cualquier pero es la que se usa por convencion para este tipo de acciones mediante PHP/MySQL.
+
 -->
 
 <?php
@@ -17,23 +19,26 @@ Aclaraciones: En este archivo vamos a hacer las conexiones  que nos van a servir
         -----------------------------------------*/
 
         public static function mdlMostrarAdministrador($tabla,$item,$valor)
-        {
+        {   
+            //El if preguntara si $item no esta vacio, por ejemplo el mail, entonces que haga la llamada a la tabla que posteriormente retornara los valores del item consultado.
+
             if($item !=null)
             {
-                $stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE $item = :$item ORDER BY id DESC");
+                $stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE $item = :$item ORDER BY id DESC"); //CONSULTA 1 (1*)
 
-                $stmt->binParam(":" .$item, $valor, PDO::PARAM_STR);
-                $stmt->execute();
+                $stmt->bindParam(":" .$item, $valor, PDO::PARAM_STR); //Enlaza variables como parámetros a una sentencia preparada
+                $stmt->execute(); //Ejecuta la consulta
                 return $stmt->fetch(); //Retornara solo los valores consultados
             } 
             
+            //Pero si esta vacio retornara todos los valores de la tabla.
             else
             {
-                $stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla ORDER BY id DESC"); 
+                $stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla ORDER BY id DESC"); //CONSULTA 2
 
-                //En la conexion podriamos poner solo "SELECT * FROM $tabla pero le agregamos "ORDER BY id DESC" para que nos muestra desde el ultimo valor de la tabla al primero cargado.
+                //En la conexion de la consulta 1 y 2 vemos que al final tiene la sentencia "ORDER BY id DESC", podria no tenerla pero la usamos en este caso para que nos muestra desde el ultimo valor de la tabla al ultimo cargado.
 
-                $stmt = execute();
+                $stmt = execute(); //Ejecuta la consulta
                 return $stmt->fethAll(); //Retornara todos los valores
             }
         }
